1b7829e09ee15516df5366ac36b0e051c8d2b1f3
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2021-10-21 14:53:47 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Second commit is in
         </p>
         <a
           className="App-link"
